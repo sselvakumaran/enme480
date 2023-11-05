379b4ac91866277c9832adfd8c68d3993b46d471
sselvakumaran <sriman.selvakumaran@gmail.com>
2023-11-05 11:22:23 -0500

    fixes

--- lab5/scripts/lab5_func.py
+++ lab5/scripts/lab5_func.py
@@ -34,23 +34,23 @@ NUM_JOINTS = 6
 PRECISION = 7
 
 PARAMETERS_UR3e = [[0,       .15185, HALF_PI],
-              [-.24355, 0,      0],
-              [-.2132,  0,      0],
-              [0,       .13105, HALF_PI],
-              [0,       .08535, -HALF_PI],
-              [0,       .0921,  0],
-							[-.0535, 	.059, 	0]]
+  	[-.24355, 0,      0],
+    [-.2132,  0,      0],
+    [0,       .13105, HALF_PI],
+    [0,       .08535, -HALF_PI],
+    [0,       .0921,  0],
+		[-.0535, 	.059, 	0]]
 
 PARAMETERS_UR3 = [[0,       .1519, HALF_PI],
-              [-.24365, 0,      0],
-              [-.21325,  0,      0],
-              [0,       .11235, HALF_PI],
-              [0,       .08535, -HALF_PI],
-              [0,       .0819,  0],
-							[-.0535, 	.059, 	0]]
+    [-.24365, 0,      0],
+    [-.21325,  0,      0],
+    [0,       .11235, HALF_PI],
+  	[0,       .08535, -HALF_PI],
+  	[0,       .0819,  0],
+		[-.0535, 	.059, 	0]]
 
 LL = [0.152, 0.120, 0.244, 0.093, 0.213, 0.083, 0.083, 0.082, 0.0535, 0.052]
-BASE_COORD = np.array([-0.15, 0.15, 0.01], dtype=np.float64)
+BASE_COORD = np.array([-0.15, 0.15, -0.102], dtype=np.float64)
 
 ANGLE_OFFSET = np.array([HALF_PI * 2, 0, 0, -HALF_PI, 0, 0, 0], dtype=np.float64)
 
@@ -72,13 +72,13 @@ def lab_fk(theta1, theta2, theta3, theta4, theta5, theta6):
 
 	##### Your Code Starts Here #####
 	# Fill in scripts from lab3 here
-	for i in range(NUM_JOINTS):
+	for i in range(len(theta)):
 		T = np.matmul(T, trans_theta_joint(theta[i], i))
 
 	##### Your Code Ends Here #####
 	print(str(T) + "\n")
 
-	theta = theta + ANGLE_OFFSET
+	theta = theta * DEG_TO_RAD + ANGLE_OFFSET
 	# angles[0] = theta1 + np.pi
 	# angles[1] = theta2
 	# angles[2] = theta3
@@ -101,8 +101,8 @@ def inverse_kinematics(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
 	# Step 2: find x_cen, y_cen, z_cen
 	yaw = yaw_WgripDegree * DEG_TO_RAD
 	wrist_center = grip - np.array([LL[8] * cos(yaw), 
-													LL[8] * sin(yaw), 
-													0], dtype=np.float64) 
+			LL[8] * sin(yaw), 
+			0], dtype=np.float64) 
 
 	# Step 3: find theta_1
 	x, y = wrist_center[0], wrist_center[1]
@@ -110,16 +110,15 @@ def inverse_kinematics(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
 	
 	rectL = LL[1] - LL[3] + LL[5]
 	angles[0] = HALF_PI - arctan(x, y) - arcsin(rectL / d)
-	#HALF_PI - arctan(y, x) - arcsin(rectL / d)
 
 	# Step 4: find theta_6 
-		# based on theta 1 and yaw angle, equals 0 when 9 parallel to 4 and 6
+	# based on theta 1 and yaw angle, equals 0 when 9 parallel to 4 and 6
 	angles[5] = HALF_PI + angles[0] - yaw
 
 	# Step 5: find x3_end, y3_end, z3_end
 	x3 = wrist_center - [LL[6]*cos(angles[0]) - rectL * sin(angles[0]),
-											LL[6]*sin(angles[0]) + rectL * cos(angles[0]),
-																							-LL[9] - LL[7]]
+			LL[6]*sin(angles[0]) + rectL * cos(angles[0]),
+			-LL[9] - LL[7]]
 
 	# Step 6: find theta_2, theta_3, theta_4
 	L1, L3, L5 = LL[0], LL[2], LL[4]
@@ -133,6 +132,5 @@ def inverse_kinematics(xWgrip, yWgrip, zWgrip, yaw_WgripDegree):
 	print(reduce(lambda x, y: x + f" {y / DEG_TO_RAD}", angles[1:], str(angles[0] / DEG_TO_RAD)))
 
 	# obtain angles from forward kinematics function
-	angles1 = lab_fk(*(angles/DEG_TO_RAD))
-	print(angles1)
+	#_ = lab_fk(*(angles/DEG_TO_RAD))
 	return angles + ANGLE_OFFSET[:6]
